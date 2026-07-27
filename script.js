d73fd498843be8cb9b6491a3ac24d38884979aee
Shehroz Basharat <shehrozbasharat7@gmail.com>
2026-07-27 17:30:43 +0500

    add delete,edit.remove button

--- script.js
+++ script.js
@@ -4,5 +4,30 @@ const noteTitle = document.querySelector('#noteTitle');
 const noteContent = document.querySelector('#noteContent');
 const submitBtn = document.querySelector('#submit');
 const charCount = document.querySelector('#charCount');
-const notesList = document.querySelector('#notesList')
+const notesList = document.querySelector('#notesList');
 
+submitBtn.addEventListener('click', (e)=>{
+e.preventDefault()
+
+    let noteTitleValue = noteTitle.value.trim();
+    let noteContentValue = noteContent.value.trim();
+
+   let notesListBox = document.createElement('div');
+   notesListBox.classList.add('note')
+
+    notesListBox.innerHTML = ` <h3>${noteTitleValue}</h3>
+    <p>${noteContentValue}</p>
+    <button class="edit-btn">Edit</button>
+    <button class="delete-btn">Delete</button>
+    <button class="pin-btn">Pin</button>`
+
+   notesList.appendChild(notesListBox);
+
+    noteTitle.value = ''
+    noteContent.value = ''
+})
+
+noteContent.addEventListener('input', ()=>{
+    let count = noteContent.value.length;
+    charCount.textContent = `${count} Characters`
+})
